a2b0fe303668ba1a5701995f6170c30bfd226722
Inkedus <inkedus@InkedusdeMacBook-Pro.local>
2021-04-02 17:47:48 +0800

    私聊群发

--- app/Plugins/fudu/boot.php
+++ app/Plugins/fudu/boot.php
@@ -2,12 +2,13 @@
 
 namespace App\Plugins\fudu;
 
-use App\Plugins\fudu\src\Controller\GroupQunfa;
-use App\Plugins\fudu\src\Controller\IndexController;
-use Illuminate\Routing\Router;
-use Illuminate\Support\Facades\Route;
 use Dcat\Admin\Admin;
 use Dcat\Admin\Layout\Menu;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Route;
+use App\Plugins\fudu\src\Controller\GroupQunfa;
+use App\Plugins\fudu\src\Controller\PrivateQunfa;
+use App\Plugins\fudu\src\Controller\IndexController;
 
 class boot{
     
@@ -30,6 +31,12 @@ class boot{
                 // 群组群发创建
                 Route::get('/qunfa/group/create', [GroupQunfa::class,'create']);
                 Route::post('/qunfa/group', [GroupQunfa::class,'store']);
+
+                // 私聊群发视图
+                Route::get('/qunfa/private', [PrivateQunfa::class,'show']);
+                // 私聊群发创建
+                Route::get('/qunfa/private/create', [PrivateQunfa::class,'create']);
+                Route::post('/qunfa/private', [PrivateQunfa::class,'store']);
             });
         });        
     }
@@ -52,6 +59,15 @@ class boot{
                     'title'         => '群组群发',
                     'icon'          => '',
                     'uri'           => 'fuduji/qunfa/group',
+                    'parent_id'     => 100, 
+                    'permission_id' => 'administrator', // 与权限绑定
+                    'roles'         => 'administrator', // 与角色绑定
+                ],
+                [
+                    'id'            => 102, // 此id只要保证当前的数组中是唯一的即可
+                    'title'         => '私聊群发',
+                    'icon'          => '',
+                    'uri'           => 'fuduji/qunfa/private',
                     'parent_id'     => 100, 
                     'permission_id' => 'administrator', // 与权限绑定
                     'roles'         => 'administrator', // 与角色绑定
