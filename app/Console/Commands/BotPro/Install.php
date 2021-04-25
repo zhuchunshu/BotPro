adf72895461cafbcf19361738a7e8c6f8cf447c7
Inkedus <inkedus@InkedusdeMacBook-Pro.local>
2021-04-25 16:31:54 +0800

    优化: 安装

--- app/Console/Commands/BotPro/Install.php
+++ app/Console/Commands/BotPro/Install.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands\BotPro;
 
 use App\Models\Option;
+use Dcat\Admin\Models\Administrator;
+use Illuminate\Support\Str;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Artisan;
@@ -47,22 +49,12 @@ class Install extends Command
                 $database['name'] = $this->ask('数据库名');
                 $database['user'] = $this->ask('数据库用户名');
                 $database['pwd'] = $this->ask('数据库密码');
-                $smtp = [];
-                $smtp['host'] = $this->ask('smtp主机地址(smtp.qq.com)');
-                $smtp['port'] = $this->ask('smtp端口(465,25或其他)');
-                $smtp['user'] = $this->ask('smtp发信邮箱用户名(example@qq.com)');
-                $smtp['pwd'] = $this->ask('smtp发信邮箱密码(授权码)');
-                $smtp['en'] = $this->ask('smtp认证方式(ssl、tsl)');
                 $env = [
                     'APP_URL' => $web_url,
                     'DB_DATABASE' => $database['name'],
                     'DB_USERNAME' => $database['user'],
                     'DB_PASSWORD' => $database['pwd'],
-                    'MAIL_HOST' => $smtp['host'],
-                    'MAIL_PORT' => $smtp['port'],
-                    'MAIL_USERNAME' => $smtp['user'],
-                    'MAIL_PASSWORD' => $smtp['pwd'],
-                    'MAIL_ENCRYPTION' => $smtp['en'],
+                    'ADMIN_ROUTE_PREFIX' => Str::random(8),
                     'APP_DEBUG' => 'false'
                 ];
                 $this->modifyEnv($env);
@@ -73,6 +65,11 @@ class Install extends Command
                 Artisan::call('migrate --force');
                 $this->info('数据库迁移成功!');
                 Artisan::call('admin:install');
+                $pwd = Str::random(12);
+                Administrator::where('username','admin')->update([
+                    'password' => bcrypt($pwd),
+                    'updated_at' => date("Y-m-d H:i:s")
+                ]);
                 $this->info('后台管理员账号创建成功!');
                 $d['BOT_TOKEN'] = $this->ask('机器人连接秘钥');
                 $d['BOT_HTTP'] = $this->ask('机器人http连接地址(http://127.0.0.1:5700/)');
@@ -85,11 +82,11 @@ class Install extends Command
                     ]);
                 }
                 $this->info('必要设置项创建成功!');
-                File::put(base_path() . DIRECTORY_SEPARATOR . 'install.lock', 'CodeFec 已安装');
+                File::put(base_path() . DIRECTORY_SEPARATOR . 'install.lock', 'BotPro 已安装');
                 $this->info('本次安装结束。');
-                $this->info('访问: 域名/admin 进入后台!');
+                $this->info("访问: 域名/".config('admin.route.prefix')." 进入后台!");
                 $this->info('默认账号: admin');
-                $this->info('默认密码: admin');
+                $this->info('密码: '.$pwd);
             }
         }else{
             $this->error('PHP版本必须大于7.3.0');
